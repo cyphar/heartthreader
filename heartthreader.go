7942a9140d6db1b57fd7ad8f97c634f43ba28a5a
cyphar <cyphar@cyphar.com>
2014-04-09 19:40:43 +1000

    heartthreader: made multithreading godlike

--- heartthreader.go
+++ heartthreader.go
@@ -1,4 +1,4 @@
-/* heartthreader: test large multithreaded amounts of domains against the Heartbleed vuln (CVE-2014-0160)
+/* heartthreader: multithreaded testing of domains against the Heartbleed vuln (CVE-2014-0160)
  * Copyright (C) 2014, Cyphar All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -30,48 +30,24 @@
 package main
 
 import (
-	"fmt"
 	"os"
-	"time"
 	"bufio"
 	"strings"
+	"sync"
+	"fmt"
+	"time"
 
 	"github.com/titanous/heartbleeder/tls"
 )
 
-func wait_err(conn *tls.Conn, ch chan error) {
-	_, _, err := conn.Heartbeat(32, nil)
-	ch <- err
+type Server struct {
+	Host string
+	Vulnerable bool
 }
 
-func check_host(host string) {
-	conn, err := tls.Dial("tcp", host, &tls.Config{InsecureSkipVerify: true})
+const NumDigesters = 20
 
-	if err != nil {
-		fmt.Printf("ERR: %s\n", err.Error())
-		return
-	}
-
-	quit := time.After(10 * time.Second)
-	ech := make(chan error)
-
-	go wait_err(conn, ech)
-
-	select {
-		case err = <-ech:
-			switch err {
-				case nil:
-					// VULNERABLE!!
-					fmt.Printf("%s\n", host)
-				case tls.ErrNoHeartbeat:
-				default:
-			}
-		case <-quit:
-			return
-	}
-}
-
-func fix_line(line string) string {
+func FixLine(line string) string {
 	if strings.HasSuffix(line, "\n") {
 		l := len(line)
 		line = line[:l-1]
@@ -84,29 +60,118 @@ func fix_line(line string) string {
 	return line
 }
 
+func YieldTargets(done <-chan struct{}, files []string) <-chan string {
+	hosts := make(chan string)
+
+	go func() {
+		defer close(hosts)
+		for _, file := range files {
+			f, err := os.Open(file)
+
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "E: %s\n", err.Error())
+				continue
+			}
+
+			bf := bufio.NewReader(f)
+
+			var line string
+			for line, err = bf.ReadString('\n'); err == nil; line, err = bf.ReadString('\n') {
+				host := FixLine(line)
+				select {
+					case hosts <- host:
+					case <-done:
+						fmt.Fprintf(os.Stderr, "I: yielding canceled\n")
+						return
+				}
+			}
+		}
+	}()
+
+	return hosts
+}
+
+func DigestTarget(done <-chan struct{}, hosts <-chan string, c chan<- Server) {
+	for host := range hosts {
+		wait_err := func(conn *tls.Conn, ch chan<- error) {
+			_, _, err := conn.Heartbeat(32, nil)
+			ch <- err
+		}
+
+		conn, err := tls.Dial("tcp", host, &tls.Config{InsecureSkipVerify: true})
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "E: %s\n", err.Error())
+			continue
+		}
+
+		quit := time.After(10 * time.Second)
+		ech := make(chan error)
+
+		go wait_err(conn, ech)
+
+		srv := Server{host, false}
+
+		select {
+			case err = <-ech:
+				switch err {
+					case nil:
+						// VULNERABLE!!
+						srv.Vulnerable = true
+					case tls.ErrNoHeartbeat:
+					default:
+				}
+			case <-quit:
+			case <-done:
+				fmt.Fprintf(os.Stderr, "I: digest of %s canceled\n", host)
+				return
+		}
+
+		select {
+			case c <- srv:
+			case <-done:
+				fmt.Fprintf(os.Stderr, "I: digest of %s canceled\n", host)
+				return
+		}
+	}
+}
+
+func DigestAll(files []string) {
+	servers := make(chan Server)
+	done := make(chan struct{})
+	defer close(done)
+
+	hosts := YieldTargets(done, files)
+
+	var wg sync.WaitGroup
+	wg.Add(NumDigesters)
+
+	for i := 0; i < NumDigesters; i++ {
+		go func() {
+			DigestTarget(done, hosts, servers)
+			wg.Done()
+		}()
+	}
+
+	go func() {
+		wg.Wait()
+		close(servers)
+	}()
+
+	for server := range servers {
+		if server.Vulnerable {
+			fmt.Printf("V: %s\n", server.Host)
+		} else {
+			fmt.Printf("N: %s\n", server.Host)
+		}
+	}
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Printf("Usage: %s hostfile[s]\n", os.Args[0])
 		os.Exit(2)
 	}
 
-	for _, file := range os.Args[1:] {
-		f, err := os.Open(file)
-
-		if err != nil {
-			fmt.Printf("ERR: %s\n", err.Error())
-			continue
-		}
-
-		bf := bufio.NewReader(f)
-
-		var line string
-		for line, err = bf.ReadString('\n'); err == nil; line, err = bf.ReadString('\n') {
-			host := fix_line(line)
-			go check_host(host)
-		}
-	}
-
-	/* block the night away */
-	select {}
+	DigestAll(os.Args[1:])
 }
