41a1dd3a82b9353fff6cfa8e3a7289b347ee6893
cyphar <cyphar@cyphar.com>
2014-05-29 21:36:31 +1000

    heartthreader: fixed LICENSE printing shambles

--- heartthreader.go
+++ heartthreader.go
@@ -47,33 +47,33 @@ type Server struct {
 	Vulnerable bool
 }
 
-const LICENSE = "heartthreader: mass, multithreaded testing for servers against Heartbleed (CVE-2014-0160)\n" +
-	"Copyright (C) 2014, Cyphar All rights reserved.\n" +
-	"\n" +
-	"Redistribution and use in source and binary forms, with or without\n" +
-	"modification, are permitted provided that the following conditions are met:\n" +
-	"\n" +
-	"1. Redistributions of source code must retain the above copyright notice,\n" +
-	"   this list of conditions and the following disclaimer.\n" +
-	"\n" +
-	"2. Redistributions in binary form must reproduce the above copyright notice,\n" +
-	"   this list of conditions and the following disclaimer in the documentation\n" +
-	"   and/or other materials provided with the distribution.\n" +
-	"\n" +
-	"3. Neither the name of the copyright holder nor the names of its contributors\n" +
-	"   may be used to endorse or promote products derived from this software without\n" +
-	"   specific prior written permission.\n" +
-	"\n" +
-	"THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\"\n" +
-	"AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE\n" +
-	"IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE\n" +
-	"DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE\n" +
-	"FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL\n" +
-	"DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR\n" +
-	"SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER\n" +
-	"CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,\n" +
-	"OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE\n" +
-	"USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.\n"
+const LICENSE = `heartthreader: mass, multithreaded testing for servers against Heartbleed (CVE-2014-0160)
+Copyright (C) 2014, Cyphar All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+   may be used to endorse or promote products derived from this software without
+   specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.`
 
 var Config struct {
 	NumThreads int
@@ -203,7 +203,7 @@ func DigestAll(files []string) <-chan Server {
 }
 
 func License() {
-	fmt.Printf(LICENSE)
+	fmt.Println(LICENSE)
 }
 
 func Help(out io.Writer) {
